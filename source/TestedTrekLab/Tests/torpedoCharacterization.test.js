e02cc60cc23ae0e6fe6702398f2f9c8e230a21a3
Rob Myers <Rob.Myers@agileInstitute.com>
2014-09-27 12:28:23 -0700

    ported torpedo characterization tests
    currently running.  Next step:  more embedded describe()s

--- source/TestedTrekLab/Tests/torpedoCharacterization.test.js
+++ source/TestedTrekLab/Tests/torpedoCharacterization.test.js
@@ -1,78 +1,88 @@
+describe("photons", function() {
+    var game;
+    var ui;
 
-
-module("photon", {
-    setup: function() {
-        // given
+    beforeEach(function() {
         game = new Game();
         ui = new UserInterface("photon");
-        sinon.stub(ui, "writeLine");
-        sinon.stub(game, "generator", function() { return 1; }); // Note: without this the test would often fail
-    }
+        spyOn(ui, "writeLine");
+        spyOn(game, "generator").and.returnValue(1);
+
+    });
+
+    it ("notifies you when no torpedoes remain", function() {
+        // given
+        game.t = 0;
+        ui.target = new Klingon();
+
+        // when
+        game.processCommand(ui);
+
+        // then
+        expect(ui.writeLine).toHaveBeenCalledWith("No more photon torpedoes!");
+    });
+
+    describe("when drift over distance causes torpedo to miss", function() {
+        beforeEach(function() {
+            var distanceWhereRandomFactorsHoldSway = 3000;
+            ui.target = new Klingon(distanceWhereRandomFactorsHoldSway, 200);
+
+            game.processCommand(ui);
+        });
+
+        it("reports torpedo missed", function() {
+            expect(ui.writeLine).toHaveBeenCalledWith("Torpedo missed Klingon at 3000 sectors...");
+        });
+
+        it("reduces torpedoes available", function() {
+            expect(game.t).toBe(7);
+        });
+    });
+
+    describe("how photon always misses when Klingon is quite far away, presumably due to clever evasive actions", function() {
+        beforeEach(function() {
+            var distanceWhereTorpedoesAlwaysMiss = 3500;
+            ui.target = new Klingon(distanceWhereTorpedoesAlwaysMiss, 200);
+
+            game.processCommand(ui);
+        });
+
+        it("reports torpedo missed", function() {
+            expect(ui.writeLine).toHaveBeenCalledWith("Torpedo missed Klingon at 3500 sectors...");
+        });
+
+        it("reduces torpedoes available", function() {
+            expect(game.t).toBe(7);
+        });
+    });
+
+    it("destroys Klingon", function() {
+        // given
+        var klingon = new Klingon(500, 200);
+        spyOn(klingon, "destroy");
+        ui.target = klingon;
+
+        // when
+        game.processCommand(ui);
+
+        // then
+        expect(ui.writeLine).toHaveBeenCalledWith("Photons hit Klingon at 500 sectors with 850 units");
+        expect(ui.writeLine).toHaveBeenCalledWith("Klingon destroyed!");
+        expect(game.t).toBe(7);
+        expect(klingon.destroy).toHaveBeenCalled();
+    });
+
+    it("damages Klingon", function() {
+        // given
+        ui.target = new Klingon(500, 2000);
+
+        // when
+        game.processCommand(ui);
+
+        // then
+        expect(ui.writeLine).toHaveBeenCalledWith("Photons hit Klingon at 500 sectors with 850 units");
+        expect(ui.writeLine).toHaveBeenCalledWith("Klingon has 1150 remaining");
+        expect(game.t).toBe(7);
+    });
 });
 
-test("Notified if no torpedoes remain", function() {
-    // given
-    game.t = 0;
-    ui.target = new Klingon();
-
-    // when
-    game.processCommand(ui);
-
-    // then
-    equal(ui.writeLine.args[0], "No more photon torpedoes!", "message");
-});
-
-test("Torpedo misses due to random factors", function() {
-    // given
-    var distanceWhereRandomFactorsHoldSway = 3000;
-    ui.target = new Klingon(distanceWhereRandomFactorsHoldSway, 200);
-
-    // when
-    game.processCommand(ui);
-
-    // then
-    equal(ui.writeLine.args[0], "Torpedo missed Klingon at 3000 sectors...", "message");
-    equal(game.t, 7, "torpedo count");
-});
-
-test("Torpedo misses due to distance and clever klingon evasive actions", function() {
-    // given
-    var distanceWhereTorpedoesAlwaysMiss = 3500;
-    ui.target = new Klingon(distanceWhereTorpedoesAlwaysMiss, 200);
-
-    // when
-    game.processCommand(ui);
-
-    // then
-    equal(ui.writeLine.args[0], "Torpedo missed Klingon at 3500 sectors...", "message");
-    equal(game.t, 7, "torpedo count");
-});
-
-test("Torpedo destroys klingon", function() {
-    // given
-    var klingon = new Klingon(500, 200);
-    sinon.spy(klingon, "destroy");
-    ui.target = klingon;
-
-    // when
-    game.processCommand(ui);
-
-    // then
-    equal(ui.writeLine.getCall(0).args[0], "Photons hit Klingon at 500 sectors with 850 units", "first message");
-    equal(ui.writeLine.getCall(1).args[0], "Klingon destroyed!", "second message");
-    equal(game.t, 7, "torpedo count");
-    ok(klingon.destroy.calledOnce, "klingon destroyed");
-});
-
-test("Torpedo damages klingon", function() {
-    // given
-    ui.target = new Klingon(500, 2000);
-
-    // when
-    game.processCommand(ui);
-
-    // then
-    equal(ui.writeLine.getCall(0).args[0], "Photons hit Klingon at 500 sectors with 850 units", "first message");
-    equal(ui.writeLine.getCall(1).args[0], "Klingon has 1150 remaining", "second message");
-    equal(game.t, 7, "torpedo count");
-});
