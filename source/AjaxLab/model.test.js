d16fa683d4639cbb22a7531406f2f46e2a2a9db7
Rob Myers <Rob.Myers@agileInstitute.com>
2014-09-28 10:44:43 -0700

    Altered AJAX sample to use a nested describe
    Downsides?

--- source/AjaxLab/model.test.js
+++ source/AjaxLab/model.test.js
@@ -1,8 +1,7 @@
-describe('model', function () {
+describe("model", function () {
     var model;
 
     beforeEach(function () {
-        // given
         model = new Model();
     });
 
@@ -10,52 +9,55 @@ describe('model', function () {
         $.mockjaxClear();
     });
 
-    it("getProduct", function (done) {
-        // given
-        $.mockjax({
-            // mockjax input
-            url: '/product',
-            data: { productId: 4711},
-            // mockjax output
-            responseText: { error: false, product: { name: 'banana'}},
-            responseTime: 1
+    describe("getProduct", function() {
+        it("retrieves product info", function (done) {
+            // given
+            $.mockjax({
+                // mockjax input
+                url: '/product',
+                data: { productId: 4711},
+                // mockjax output
+                responseText: { error: false, product: { name: 'banana'}},
+                responseTime: 1
+            });
+
+            // when
+            model.getProduct(4711, function (error, product) {
+
+                // then
+                expect(error).toBe(null);
+                expect(product.name).toBe('banana');
+                done(); // important for async test, must be called in final callback
+            });
         });
 
-        // when
-        model.getProduct(4711, function (error, product) {
+        it("reports product-not-found server error", function (done) {
+            // given
+            $.mockjax({
+                url: '/product',
+                data: { productId: 17},
+                responseText: { error: { message: "product with id 17 not found"}},
+                responseTime: 1
+            });
 
-            // then
-            expect(error).toBe(null);
-            expect(product.name).toBe('banana');
-            done(); // important for async test, must be called in final callback
+            // when
+            model.getProduct(17, function (error, product) {
+
+                // then
+                expect(error.message).toBe("product with id 17 not found");
+                done();
+            });
+        });
+
+        it("handles unresponsive server", function (done) {
+            // when
+            model.getProduct(17, function (error, product) {
+
+                // then
+                expect(error.message).toBe("server connection failed");
+                done();
+            });
         });
     });
 
-    it("getProduct when server returns an error", function (done) {
-        // given
-        $.mockjax({
-            url: '/product',
-            data: { productId: 17},
-            responseText: { error: { message: "product with id 17 not found"}},
-            responseTime: 1
-        });
-
-        // when
-        model.getProduct(17, function (error, product) {
-
-            // then
-            expect(error.message).toBe("product with id 17 not found");
-            done();
-        });
-    });
-
-    it("getProduct when server unresponsive", function (done) {
-        // when
-        model.getProduct(17, function (error, product) {
-
-            // then
-            expect(error.message).toBe("server connection failed");
-            done();
-        });
-    });
 });
